bc0e523ada0132399af4036b1b5aef4557569f6a
Chuang Chen <chuangchen@Chuang-Chens-MacBook-Pro.local>
2020-12-18 13:42:45 +0800

    change server

--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,8 @@ import io from 'socket.io-client';
 import {changeBgColor, changeBgPos} from './index2.js';
 import {showShape, showText, showGraph} from './sketch.js';
 import {newSound, changeFilter} from './sound.js';
-var serverIP = 'https://tutorial-playground-server.herokuapp.com';
-//var serverIP = 'https://osc-server-template.glitch.me/';
+//var serverIP = 'https://tutorial-playground-server.herokuapp.com';
+var serverIP = 'https://osc-server-template.glitch.me/';
 var socket;
 connectServer('');
 
